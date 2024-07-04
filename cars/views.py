bd6699ddfd92d1cfb50bc20179004c62857727b8
Lucas Moura <154241503+llucasmoura2@users.noreply.github.com>
2024-07-03 21:38:17 -0300

    Atualizar o views.py

--- cars/views.py
+++ cars/views.py
@@ -30,43 +30,8 @@ class CarListViews(ListView):
 
 
 
-"""""
-def cars_view(request):
-    cars= Car.objects.all().order_by('model')  # .order_by(" - model") a ordenação fica ao contrario 
-    search = request.GET.get('search')
-    
-    if search:
-        cars = Car.objects.filter(model__icontains = search)
-    
-    return render (request, 
-                   'cars.html', 
-                {'cars':cars}
-     )
 
-     #modelo function views pode ser substituido por class views 
-     #modelo class view abaixo
-"""""
-
-# cars = Car.objects.all()
-#cars = Car.objects.filter(brand__name = 'Fiat') #ou "Car.objects.filter(brand = 2)" o id é a posição na tabela sql "foreingkey"
-# cars = Car.objects.filter(model = "uno com escada") desse jeito se não for uma ForeingKey
- # cars = Car.objects.filter(model__contains = "chevette") "contains" significa que contem e pode ser uma letra ou palavra 
-
-
-
-
-"""""
-class CarsView(View):
-    def get(self, request):
-        cars= Car.objects.all().order_by('model')  # .order_by(" - model") a ordenação fica ao contrario 
-        search = request.GET.get('search')
-    
-        if search:
-            cars = Car.objects.filter(model__icontains = search)
-    
-        return render(request, 'cars.html', {'cars':cars})
-
----------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------
 
 
 def new_car_view(request):
@@ -79,10 +44,8 @@ def new_car_view(request):
        new_car_form = CarModelForm
     return render (request, 'new_car.html', {'new_car_form': new_car_form})
 
-"""""
 
 
-"""""
 class NewCarView(View):
     def get(self, request):
         new_car_form = CarModelForm()
@@ -95,7 +58,7 @@ class NewCarView(View):
             return redirect('cars_list')
         return render (request, 'new_car.html', {'new_car_form': new_car_form})
 
-"""""
+
 
 @method_decorator(login_required(login_url= 'login'), name = 'dispatch')
 class NewCarCreatView(CreateView):
